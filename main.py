4df9317ef7d346553bab373939311d22bb9b1755
Mohini Bhave <mb5157@columbia.edu>
2024-05-05 14:44:59 -0400

    Updated main

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ wandb.init(mode="disabled")
 from model.quantization import ModelQuantizer
 from model.student_train import train_student
 from model.teacher_train import train_teacher
-from evaluation.benchmark import ModelEvaluator
+from evaluation.benchmark import ModelBenchmark  # Updated class name
 from model.distillation import perform_model_distillation
 from model.onnx_model import convert_to_onnx
-from plots.plot import plot_model_data
+from plots.plot import plot_model_performance, plot_accuracy_and_latency, plot_top_categories  # Updated for new plotting functions
 from optimum.onnxruntime import ORTModelForFeatureExtraction
 
 # Setting up environment variable to avoid parallelism issues with tokenizers
@@ -32,10 +32,11 @@ test_data = ag_news_dataset["test"]
 student_model = train_student("sentence-transformers/paraphrase-MiniLM-L3-v2", sampled_train_data)
 teacher_model = train_teacher("sentence-transformers/paraphrase-mpnet-base-v2", sampled_train_data)
 
-# Evaluate both models
-evaluator = ModelEvaluator(student_model, test_data)
-student_benchmark = evaluator.conduct_benchmark()
-teacher_benchmark = evaluator.conduct_benchmark(teacher_model, test_data)
+# Evaluate both models using the updated benchmark class
+benchmark_evaluator = ModelBenchmark(student_model, test_data)
+student_benchmark = benchmark_evaluator.run_benchmark()
+benchmark_evaluator.model = teacher_model  # Update model in the evaluator for re-use
+teacher_benchmark = benchmark_evaluator.run_benchmark()
 
 # Perform and evaluate model distillation
 distilled_student_model = perform_model_distillation(student_model, teacher_model, student_training_subset)
@@ -46,16 +47,16 @@ model_directory = Path("distilled")
 converted_model, converted_tokenizer = convert_to_onnx(model_directory)
 
 # Benchmarking non-quantized ONNX model
-onnx_model_evaluator = ModelEvaluator(converted_model, test_data)
-non_quantized_benchmark = onnx_model_evaluator.conduct_benchmark_onnx("onnx/model.onnx")
+onnx_benchmark_evaluator = ModelBenchmark(converted_model, test_data)
+non_quantized_benchmark = onnx_benchmark_evaluator.run_benchmark()
 
 # Quantize the ONNX model
 onnx_quantizer = ModelQuantizer("onnx", student_model.model_head, converted_tokenizer, test_data)
 quantized_onnx_model = onnx_quantizer.quantize_model()
 
 # Load and benchmark the quantized ONNX model
-quantized_model_evaluator = ModelEvaluator(quantized_onnx_model, test_data)
-quantized_model_benchmark = quantized_model_evaluator.conduct_benchmark_onnx("onnx/model_quantized.onnx")
+quantized_benchmark_evaluator = ModelBenchmark(quantized_onnx_model, test_data)
+quantized_model_benchmark = quantized_benchmark_evaluator.run_benchmark()
 
 # Final results output
 results = {
